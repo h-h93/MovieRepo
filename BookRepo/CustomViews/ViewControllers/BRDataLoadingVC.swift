c779cb2bb83b77aaf7d06563267a9ac10d780dcf
h-93 <hanif234@hotmail.com>
2024-03-18 20:52:24 +0000

    Created loading view

---
+++ BookRepo/CustomViews/ViewControllers/BRDataLoadingVC.swift
@@ -0,0 +1,47 @@
+//
+//  BRLoadingView.swift
+//  MovieRepo
+//
+//  Created by hanif hussain on 18/03/2024.
+//
+
+import UIKit
+
+class BRDataLoadingView: UIViewController {
+    
+    var containerView: UIView!
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.2) {
+            self.containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(activityIndicator)
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async { [weak self] in
+            self?.containerView.removeFromSuperview()
+            self?.containerView = nil
+            
+        }
+    }
+    
+
+}
